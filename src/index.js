c19275105fd9ad78d1386f3ee061141ca6ae9112
Ben Hong <ben@bencodezen.io>
2020-01-18 11:12:18 -0500

    feature: Add ability to output JSON files

--- src/index.js
+++ src/index.js
@@ -5,3 +5,7 @@ let rawData = fs.readFileSync('test.json')
 let jsonData = JSON.parse(rawData)
 
 console.log(jsonData)
+
+// Write data to file
+let output = JSON.stringify(jsonData)
+fs.writeFileSync('output.json', output)
