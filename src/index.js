10c372a9f003ed107b025bfc64dcb0915579065d
Ben Hong <ben@bencodezen.io>
2020-01-22 22:58:29 -0500

    feature: Prototype of AWS Transcribe conversion

--- src/index.js
+++ src/index.js
@@ -1,11 +1,36 @@
 const fs = require('fs')
 
-// Get data from file
-let rawData = fs.readFileSync('test.json')
-let jsonData = JSON.parse(rawData)
+// Declare final output
+const output = {
+  transcript: []
+}
 
-console.log(jsonData)
+// Get data from file
+const rawData = fs.readFileSync('testAsrOutput.json')
+const jsonData = JSON.parse(rawData)
+
+// Get metadata
+const items = jsonData.results.items
+const segments = jsonData.results.speaker_labels.segments
+
+// TODO: Loop through ever segment to generate time stamps and speaker
+segments.forEach(segment => {
+  output.transcript.push({
+    startTime: segment.start_time,
+    endTime: segment.end_time,
+    speaker: segment.speaker_label
+  })
+})
+
+// TODO: Loop through items to generate lines
+output.transcript.forEach(block => {
+  // If start time matches item in transcript
+  // Start adding content into line property
+  // If end time matches item, prepare to end line generation
+  // If punctuation, add to current block
+  // Otherwise,
+})
 
 // Write data to file
-let output = JSON.stringify(jsonData)
-fs.writeFileSync('output.json', output)
+let outputString = JSON.stringify(output)
+fs.writeFileSync('output.json', outputString)
